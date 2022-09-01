0628134ffd26f0eb88a83d6250e7444bf25fe6c1
marcokhairy <marcomark5050@gmail.com>
2022-09-01 11:55:45 +0200

    modified:   style.css

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const progress70 = document.querySelector(".container3");
 
 const Creative = document.querySelector(".skillfour");
 const progress85 = document.querySelector(".container4");
+const sec1 = document.querySelector("#About");
+const sec2 = document.querySelector("#projects");
+const sec3 = document.querySelector("#skills");
+const sec4 = document.querySelector("#contact");
 
 HTML.addEventListener("mouseover", function () {
   progress90.style.display = "block";
@@ -37,3 +41,82 @@ Creative.addEventListener("mouseover", function () {
 Creative.addEventListener("mouseout", function () {
   progress85.style.display = "none";
 });
+window.addEventListener("scroll", function () {
+  reveal();
+});
+function nav() {
+  sec1.scrollIntoView({ behavior: "smooth" });
+}
+function reveal() {
+  const sec1Pos = sec1.getBoundingClientRect().top;
+  const sec2Pos = sec2.getBoundingClientRect().top;
+  const sec3Pos = sec3.getBoundingClientRect().top;
+  const sec4Pos = sec4.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+  const number = 150;
+  if (sec1Pos < windowHeight - number) {
+    sec1.style.display = "block";
+  } else {
+    sec1.style.display = "none";
+  }
+  if (sec2Pos < windowHeight - number) {
+    sec2.style.display = "block";
+  } else {
+    sec2.style.display = "none";
+  }
+  if (sec3Pos < windowHeight - number) {
+    sec3.style.display = "block";
+  } else {
+    sec3.style.display = "none";
+  }
+  if (sec4Pos < windowHeight - number) {
+    sec4.style.display = "flex";
+  } else {
+    sec4.style.display = "none";
+  }
+}
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}

6eacd59467851710dc9dc5f1f0fa4efa8d473c4c
marcokhairy <marcomark5050@gmail.com>
2022-11-05 21:55:05 +0200

    s

--- main.js
+++ main.js
@@ -9,12 +9,19 @@ const progressList = Array.from(document.querySelectorAll(".containers"));
 const overlay = document.createElement("div");
 const cert1 = document.querySelector("#cert1");
 const cert2 = document.querySelector("#cert2");
-const img = document.createElement("img");
+const menu = document.querySelector(".menu-btn__container");
+const menuBTN = document.querySelector(".menu-btn__burger");
+const sideBar = document.querySelector(".SideMenu");
+const sideBarBTNs = Array.from(document.querySelectorAll(".SideBarBtn"));
+let menuOpen = false;
 // ========================================
 //              CREATED ELEMENTS
 //==========================================
+const img = document.createElement("img");
 overlay.appendChild(img);
 overlay.classList.add("blackout");
+const clickArea = document.createElement("div");
+document.body.appendChild(clickArea);
 // ========================================
 //               FUNCTIONS
 //==========================================
@@ -28,10 +35,14 @@ const nav = (num, section) => {
   }
 };
 const hideNav = () => {
-  navBar.style.opacity = "0";
-  navBar.querySelector(".navlist").style.display = "none";
-  navBar.querySelector(".logo").style.opacity = "0";
-  navBar.style.backdropFilter = "none";
+  if (!clientWidth.matches) {
+    navBar.style.opacity = "0";
+    navBar.querySelector(".navlist").style.opacity = "0";
+    navBar.querySelector(".logo").style.opacity = "0";
+    navBar.style.backdropFilter = "none";
+  } else {
+    return;
+  }
 };
 const reveal = () => {
   const windowHeight = window.innerHeight;
@@ -54,6 +65,13 @@ const revealMOB = () => {
       element.classList.remove("active");
     }
   });
+};
+const closeMenu = () => {
+  sideBar.classList.add("inactive");
+  sideBar.style.transform = "translatex(-200px)";
+  menuOpen = false;
+  menu.classList.remove("open");
+  clickArea.style.transform = "translatex(-500px)";
 };
 // ========================================
 //               EventListener
@@ -79,15 +97,37 @@ navBar.addEventListener("mouseout", () => {
   hideNav();
 });
 navBar.addEventListener("mouseover", () => {
-  navBar.style.opacity = "1";
-  navBar.querySelector(".navlist").style.display = "flex";
-  navBar.querySelector(".logo").style.opacity = "1";
-  navBar.style.backdropFilter = "blur(50px)";
+  if (!clientWidth.matches) {
+    navBar.style.opacity = "1";
+    navBar.querySelector(".navlist").style.opacity = "1";
+    navBar.querySelector(".logo").style.opacity = "1";
+    navBar.style.backdropFilter = "blur(50px)";
+  } else {
+    return;
+  }
 });
+menu.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuOpen = true;
+    menu.classList.add("open");
+    sideBar.classList.remove("inactive");
+    sideBar.style.transform = "translatex(0px)";
+    sideBar.style.backdropFilter = "blur(70px)";
+    clickArea.classList.add("clickArea");
+    clickArea.style.transform = "translatex(0px)";
+  } else {
+    closeMenu();
+  }
+});
+clickArea.addEventListener("click", () => {
+  closeMenu();
+});
+
 // ========================================
 //               if statements
 //==========================================
 if (clientWidth.matches) {
+  menu.classList.remove("inactive");
   window.setInterval(() => {
     {
       revealMOB();
@@ -111,7 +151,23 @@ skillsList.forEach((element) => {
     progressList[skillsList.indexOf(element)].style.display = "none";
   });
 });
-
+sideBarBTNs.forEach((button) => {
+  button.addEventListener("click", () => {
+    closeMenu();
+  });
+  button.addEventListener("mouseover", () => {
+    button.querySelector(".icon").style.transform = "translatex(-50px)";
+    button.querySelector(".HiddenText").style.opacity = "1";
+    button.querySelector(".HiddenText").style.transform =
+      "translate(10px, 5px)";
+  });
+  button.addEventListener("mouseout", () => {
+    button.querySelector(".icon").style.transform = "translatex(0px)";
+    button.querySelector(".HiddenText").style.opacity = "0";
+    button.querySelector(".HiddenText").style.transform =
+      "translate(10px, 42px)";
+  });
+});
 //FOR THE FORM//
 window.addEventListener("DOMContentLoaded", () => {
   // get the form elements defined in your form HTML above
